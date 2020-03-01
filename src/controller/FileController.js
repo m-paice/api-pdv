9906cb9e0a8a641965bf8ae30a3f0e553a663283
Matheus Paice <matheus.paice@gmail.com>
2020-03-01 17:05:27 -0300

    update avatar

--- src/controller/FileController.js
+++ src/controller/FileController.js
@@ -1,8 +1,9 @@
 const multer = require('multer');
 
 const BaseController = require('./BaseController');
-const fileResource = require('../resource/FileResource');
 const configMulter = require('../middleware/multer');
+const fileResource = require('../resource/FileResource');
+const authorResource = require('../resource/AuthorResource');
 
 class FileController extends BaseController {
   constructor() {
@@ -32,10 +33,47 @@ class FileController extends BaseController {
     }
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  async update(req, res) {
+    const { query } = req;
+    const { id } = req.params;
+    const { type, authorId = null, postId = null } = req.body;
+    const { mimetype, filename, originalname } = req.file;
+
+    try {
+      const isFile = await fileResource.findById(id);
+
+      if (!isFile) {
+        return res.status(500).json({
+          message: 'File not found',
+        });
+      }
+
+      await fileResource.updateById(id, {
+        type,
+        name: filename,
+        originalName: originalname,
+        mimeType: mimetype.split('/')[0],
+        extension: mimetype.split('/')[1],
+        authorId,
+        postId,
+      });
+
+      const response = await authorResource.findById(authorId, { ...query });
+
+      return res.json(response);
+    } catch (error) {
+      return res.status(500).json({
+        message: error.toString(),
+      });
+    }
+  }
+
   routes() {
     const routes = super.routes();
 
     routes.post('/file', multer(configMulter).single('file'), this.createFile.bind(this));
+    routes.put('/file/:id', multer(configMulter).single('file'), this.updateFile.bind(this));
 
     return routes;
   }
